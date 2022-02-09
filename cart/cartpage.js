91d5224c101f82c95d52c8ba157ef5f38443a1cb
Kelly Huang <kellyhuang@KellydeMacBook-Pro.local>
2022-02-09 12:55:45 +0800

    new

--- cart/cartpage.js
+++ cart/cartpage.js
@@ -1,18 +1,30 @@
 function formCheck() {
-    var username = document.getElementsByName("username")[0].value;
-    var phonenumber=document.getElementsByName("tel")[0].value;
-    var addressvalue=document.getElementsByName("address")[0].value;
-    if (username[0] == null && phonenumber[0] == null && addressvalue[0] == null) {
-      alert("請填寫姓名,電話及地址唷！");
-      return false;
-    }
-    if ( phonenumber[0] == null && addressvalue[0] == null) {
-      alert("請填寫電話及地址唷！");
-      return false;
-    }
-    if ( addressvalue[0] == null) {
-      alert("請填寫地址唷！");
-      return false;
-    }
+  var username = document.getElementsByName("username")[0].value;
+  var phonenumber=document.getElementsByName("tel")[0].value;
+  var addressvalue=document.getElementsByName("address")[0].value;
+  if (username[0] == null && phonenumber[0] == null && addressvalue[0] == null) {
+    alert("請填寫姓名,電話及地址唷！");
+    return false;
   }
+  if ( phonenumber[0] == null && addressvalue[0] == null) {
+    alert("請填寫電話及地址唷！");
+    return false;
+  }
+  if ( addressvalue[0] == null) {
+    alert("請填寫地址唷！");
+    return false;
+  }
+}
+
+$(document).ready(function () {
+  let name = $.cookie("username");
+  $("#name").val(name);
+  let email = $.cookie("email");
+  $("#EMAIL").val(email);
+  let tel = $.cookie("tel");
+  $("#TEL").val(tel);
+  let address = $.cookie("address");
+  $("#ADDRESS").val(address);
+
+})
  
--- cart/signup.js
+++ cart/signup.js
@@ -1,29 +1,31 @@
 function formCheck() {
-    var username = document.getElementsByName("username")[0].value;
-    var email=document.getElementsByName("useremail")[0].value;
-    var addressvalue=document.getElementsByName("address")[0].value;
-    if (username[0] == null && email[0] == null && addressvalue[0] == null) {
-      alert("請填寫姓名,電話及地址唷！");
+    let username = document.getElementById("name").value;
+    console.log(username);
+    /*var email=document.getElementsByName("useremail")[0].value;
+    var addressvalue=document.getElementsByName("useraddress")[0].value;*/
+    if (username ="" ) {
+      alert("請填寫姓名唷！");
       return false;
     }
-    if ( email[0] == null && addressvalue[0] == null) {
-      alert("請填寫電話及地址唷！");
-      return false;
+    return true;
+}
+
+$("#CHECKPASSWORD").keyup(check);
+$("#password").keyup(check);
+
+function check(event) {
+    let value1 = $("#password").val();
+    let value2 = $("#CHECKPASSWORD").val();
+    if (value1.length < 8 && value2.length < 8 ){
+        $("#length").text("密碼長度需大於8個字");
+    } else {
+        $("#length").text("");
+        
     }
-   
-  }
-
-$("#CHECKPASSWORD").keyup(
-    function check(event){
-        let value1 = $("#PASSWORD").val();
-        let value2 = $(this).val();
-        console.log(value1,value2);
-        if(value1 !== value2){
-            $("#non").text("請輸入相同的密碼");
-        }else{
-            $("#non").text("");
-        }
-
+    if (value1 !== value2){
+        $("#non").text("請輸入相同的密碼");
+    } else {
+        $("#non").text("");
     }
     
-);
+}
